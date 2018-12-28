7667ac506f192fc4e31ecd9143e7af98c73a4733
gao <1417395446@qq.com>
2018-12-28 15:41:17 +0800

    添加四个onclick属性，生成空函数

--- app/src/main/java/com/example/gao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gao/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gao.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view) {
+    }
+
+    public void openFamilyList(View view) {
+    }
+
+    public void openColorsList(View view) {
+    }
+
+    public void openPhrasesList(View view) {
     }
 }
